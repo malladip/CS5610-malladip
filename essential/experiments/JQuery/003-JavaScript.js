450e29c081f85a12854c9b79097329c3cce6e73d
malladip <malladip@ccs.neu.edu>
2015-02-07 18:29:42 -0500

    experiment 3
    Login Page

---
+++ essential/experiments/JQuery/003-JavaScript.js
@@ -0,0 +1,31 @@
+﻿$(function () {
+    $("#admin").click(function () {
+        $("#admin").animate({
+            height: '180px',
+            width: '180px'
+        })
+
+        this.disabled = true;
+        $("#guest").remove();
+        
+        var username = "<input type=\"text\" id=\"username\" name=\"username\" placeholder=\"Username\" />";
+        var password = "<input type=\"password\" id=\"password\" name=\"password\" placeholder=\"**********\" />";
+
+        $("#username").css("width", "100%");
+        $("#password").css("width", "100%");
+        $("#login_content").append(username);
+        $("#login_content").append(password);
+    });
+
+
+
+    $("#guest").click(function () {
+        $("#guest").animate({
+            height: '180px',
+            width: '180px'
+        })
+
+        this.disabled = true;
+        $("#admin").remove();
+    });
+});
