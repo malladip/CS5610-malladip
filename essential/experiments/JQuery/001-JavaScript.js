15c1adf3b3ed8c78b587547999be30140e6f038e
malladip <malladip@ccs.neu.edu>
2015-02-07 14:59:56 -0500

    experiment 1
    current time changing dynamically

---
+++ essential/experiments/JQuery/001-JavaScript.js
@@ -0,0 +1,19 @@
+﻿$(function () {
+    setInterval(function () {
+        var dateObj = new Date();
+
+        var time = dateObj.toLocaleTimeString();
+
+        $("#local-time").find("#time").html(time);
+
+        var month = dateObj.getMonth();
+        $("#date").find("#month").html(month + 1);
+
+        var day = dateObj.getDay();
+        $("#date").find("#day").html(day+1);
+
+        var year = dateObj.getFullYear();
+        $("#date").find("#year").html(year);
+
+    }, 1000);
+});
