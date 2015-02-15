33f54534b6cc07f90688275be31a82de48c90b48
malladip <malladip@ccs.neu.edu>
2015-02-15 00:03:14 -0500

    Week 3

---
+++ essential/experiments/AngularJs/js/003-LocalStorage.js
@@ -0,0 +1,28 @@
+﻿$(function () {
+
+    $("#go").click(saveName);
+});
+
+
+
+function saveName() {
+    if (typeof (Storage) != "undefined") {
+        if (localStorage.name) {
+            localStorage.name = document.getElementById("name").value;
+        } else {
+            localStorage.name = "No name entered";
+        }
+        document.getElementById("message").innerHTML = "Welcome back " + localStorage.name;
+        $("#message").dialog();
+    } else {
+        document.getElementById("message").innerHTML = "Sorry, your browser does not support web storage...";
+        $("#message").dialog();
+    }
+}
+function loadName() {
+    if (localStorage.name != null) {
+        document.getElementById("message").innerHTML = "Welcome back " + localStorage.name;
+        $("#message").dialog();
+
+    }
+}
